67ae919334f36c0faddd1209a30db8f9fe027e51
leytisha-prog <lejack@syr.edu>
2026-02-08 16:44:19 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,10 @@ st.set_page_config(
 # Create pages for navigation
 HW1_page = st.Page("HWs/HW1.py", title="HW1", icon="📄")
 HW2_page = st.Page("HWs/HW2.py", title="HW2", icon="🧪")
+HW3_page = st.Page("HWs/HW3.py", title="HW3", icon="📊")
 
 
-pg = st.navigation([HW1_page, HW2_page])
+pg = st.navigation([HW1_page, HW2_page, HW3_page])
 
 st.set_page_config(page_title="HW Manager", page_icon=':material/edit:')
 pg.run() 
