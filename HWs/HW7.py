ec63fe2eb4aa72ff7512cdd6d724d0a2312fe390
leytisha-prog <lejack@syr.edu>
2026-03-22 02:54:44 +0000

    update

--- HWs/HW7.py
+++ HWs/HW7.py
@@ -1,5 +1,9 @@
-import pandas as pd
+import os
 import streamlit as st
 
-df = pd.read_csv("news.csv", sep=";")
-st.write(df.head())
+#df = pd.read_csv("news.csv", sep=";")
+#st.write(df.head())
+
+st.write("Current dircetory:", os.getcwd())
+st.write("Files here:", os.listdir())
+
